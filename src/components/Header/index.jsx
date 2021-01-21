0728584c59652cf52770f5b737c9cf215771fa78
ruben <rubenlorenzo85@yahoo.es>
2021-01-21 13:12:23 +0100

    create Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.scss";
+
+const Header = (props) =>{ 
+  return(
+    <header>
+      <h1>Kanban project</h1>
+    </header>
+  )
+}
+
+export default Header;
