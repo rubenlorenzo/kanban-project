ea9cd51529ef08ac88d9c17d97a79da567f27ee0
ruben <rubenlorenzo85@yahoo.es>
2021-02-15 15:45:33 +0100

    List convert in a React Component

--- src/components/Board/List.jsx
+++ src/components/Board/List.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import { FaPlus } from "react-icons/fa";
 
-const List = (props) => {
-  return (
-    <div
-      className="list"
-      onDragStart={(e) => props.dragStart(e, props.index, props.id)}
-      onDragOver={(e) => props.dragEnter(e, props.index, props.id)}
-      onDragEnd={props.dragEnd}
-      draggable
-    >
-      <div className="titleList">
-        <h4>{props.name}</h4>
-        <button>
-          <FaPlus />
-        </button>
+class List extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div
+        className="list"
+        onDragStart={(e) => this.props.dragStart(e, this.props.index, this.props.id)}
+        onDragOver={(e) => this.props.dragEnter(e, this.props.index, this.props.id)}
+        onDragEnd={this.props.dragEnd}
+        draggable
+      >
+        <div className="titleList">
+          <h4>{this.props.name}</h4>
+          <button>
+            <FaPlus />
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default List;
