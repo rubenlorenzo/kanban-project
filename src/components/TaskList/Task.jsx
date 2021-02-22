ac4e7404f0617deb4c886512625b79df4e1c5b68
ruben <rubenlorenzo85@yahoo.es>
2021-02-22 10:53:02 +0100

    create editTask

--- src/components/TaskList/Task.jsx
+++ src/components/TaskList/Task.jsx
@@ -1,17 +1,57 @@
 import React from "react";
+import { FaPlus, FaPen, FaReply, FaTrashAlt } from "react-icons/fa";
 
 class Task extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      name: "",
       task: props.task,
+      edit: false,
     };
   }
 
   render() {
-    const { name } = this.state.task;
-    return <li className="task">{name}</li>;
+    const { task, name, edit } = this.state;
+
+    return (
+      <li className="task">
+        {edit ? (
+          <>
+            <input
+              type="text"
+              value={name}
+              className="inputTaskNameEdit"
+              placeholder={task.name}
+              onChange={this.handleChange}
+            ></input>
+            <button className="undoEditTask" onClick={this.undoEditTask}>
+              <FaReply/>
+            </button>
+          </>
+        ) : (
+          <>
+            <span>{task.name}</span>
+            <button className="editTask" onClick={this.editTask}>
+              <FaPen />
+            </button>
+          </>
+        )}
+      </li>
+    );
+  }
+
+  handleChange = (event) => {
+    this.setState({ name: event.target.value });
+  };
+
+  editTask = (event) =>{
+    this.setState({edit:true});
+  }
+
+  undoEditTask = (event) => {
+    this.setState({edit:false});
   }
 }
 
